33e103e556f70a9c684421035241277195260146
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-07-21 15:23:19 +0200

    bun

--- typeScript/advanced-setup/super-hero/dist/main.js
+++
@@ -1,8 +0,0 @@
-import MarvelData from "./superheroes.json" with { type: "json" };
-function logSuperheroCount(data) {
-    const count = data.superheroes.length;
-    console.log(`--- Marvel Data Summary ---`);
-    console.log(`Total Superheroes Found: ${count}`);
-    console.log(`---------------------------`);
-}
-logSuperheroCount(MarvelData);
