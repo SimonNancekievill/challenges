1e19e6a74253017a3c8b4e062604d04fca2dbbe8
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-07-27 12:52:09 +0200

    adding listening and port

---
+++ expressJS/bookmark-manager-api/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+
+interface Bookmark {
+  id: number;
+  url: string;
+  title: string;
+  tag?: string;
+}
+
+let bookmarks: Bookmark[] = [
+  { id: 1, url: "https://expressjs.com", title: "Express.js", tag: "node" },
+  {
+    id: 2,
+    url: "https://typescriptlang.org",
+    title: "TypeScript",
+    tag: "typescript",
+  },
+  { id: 3, url: "https://developer.mozilla.org", title: "MDN Web Docs" },
+];
+
+const app = express();
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is runnning at http://localhost:${port}`);
+});
