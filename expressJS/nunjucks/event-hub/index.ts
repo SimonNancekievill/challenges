ddb809f023908d6f5aee9f8ad5bf08e4a99e3a17
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-07-28 15:35:07 +0200

    routing fixed

--- expressJS/nunjucks/event-hub/index.ts
+++ expressJS/nunjucks/event-hub/index.ts
@@ -12,6 +12,31 @@ app.get("/", (req, res) => {
   res.render("index.html");
 });
 
+app.get("/events", (req, res) => {
+  res.render("events.html", {
+    events: [
+      {
+        name: "React Conf",
+        date: "June 10, 2025",
+        location: "Berlin",
+        soldOut: false,
+      },
+      {
+        name: "Vue.js Summit",
+        date: "July 2, 2025",
+        location: "Amsterdam",
+        soldOut: true,
+      },
+      {
+        name: "Node.js Interactive",
+        date: "August 15, 2025",
+        location: "London",
+        soldOut: false,
+      },
+    ],
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
