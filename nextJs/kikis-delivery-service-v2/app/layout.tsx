3e3f6a4a7da44e4a56035e6444af5b7423432a75
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-09-09 12:45:38 +0200

    feat: adding custom font

--- nextJs/kikis-delivery-service-v2/app/layout.tsx
+++ nextJs/kikis-delivery-service-v2/app/layout.tsx
@@ -13,9 +13,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${cherryBomb.variable}`}>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.pumpkin.min.css"
+        />
+      </head>
       <body>
         <header>
-          <h1>Kiki's Delivery Service</h1>
+          <h1 style={{ fontFamily: "var(--font-cherry-bomb)" }}>
+            Kiki's Delivery Service
+          </h1>
         </header>
         {children}
       </body>
