bc5066f873887a176bafae16ac72823b1fb2605b
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-09-10 10:27:13 +0200

    feat: tailwindcss setup and styling button with hover

--- nextJs/kikis-delivery-service-v2/app/deliveries/new/page.tsx
+++ nextJs/kikis-delivery-service-v2/app/deliveries/new/page.tsx
@@ -5,7 +5,12 @@ export default function NewDeliveryPage() {
     <form action={addDelivery}>
       <input name="pickup" placeholder="Pickup" />
       <input name="destination" placeholder="Destination" />
-      <button type="submit"> Create request</button>
+      <button
+        type="submit"
+        className="bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+      >
+        Create request
+      </button>
     </form>
   );
 }
--- nextJs/kikis-delivery-service-v2/app/layout.tsx
+++ nextJs/kikis-delivery-service-v2/app/layout.tsx
@@ -1,3 +1,5 @@
+import "./globals.css";
+
 import { Cherry_Bomb_One } from "next/font/google";
 
 const cherryBomb = Cherry_Bomb_One({
@@ -13,12 +15,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${cherryBomb.variable}`}>
-      <head>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.pumpkin.min.css"
-        />
-      </head>
       <body>
         <header>
           <h1 style={{ fontFamily: "var(--font-cherry-bomb)" }}>
